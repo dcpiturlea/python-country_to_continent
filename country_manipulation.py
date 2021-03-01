b26390defd15720dfac35bea55946aed7e21c273
Cristian Piturlea <57861141+dcpiturlea@users.noreply.github.com>
2021-03-01 12:15:21 +0200

    Create country_manipulation.py

---
+++ country_manipulation.py
@@ -0,0 +1,11 @@
+import pycountry_convert as pc
+
+def country_to_continent(country_name):
+    country_alpha2 = pc.country_name_to_country_alpha2(country_name)
+    country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+    country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
+    return country_continent_name
+
+# Example
+#country_name = 'Romania'
+#print(country_to_continent(country_name))
